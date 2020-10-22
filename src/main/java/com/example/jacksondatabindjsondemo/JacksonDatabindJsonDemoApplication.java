dc27335f059062aa6ffa88fd25a9ee9e1822c911
bjmo <bjmo@gft.com>
2020-10-22 14:08:45 +0200

    initial commit

---
+++ src/main/java/com/example/jacksondatabindjsondemo/JacksonDatabindJsonDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jacksondatabindjsondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonDatabindJsonDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacksonDatabindJsonDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jacksondatabindjsondemo/JacksonDatabindJsonDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jacksondatabindjsondemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonDatabindJsonDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
