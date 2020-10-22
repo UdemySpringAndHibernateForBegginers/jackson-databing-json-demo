81197a7145568d81c2d8bd1ca418a428faf5465c
bjmo <bjmo@gft.com>
2020-10-22 21:23:37 +0200

    add demo code for converting simple json to java pojo

--- src/main/java/com/example/jacksondatabindjsondemo/JacksonDatabindJsonDemoApplication.java
+++ src/main/java/com/example/jacksondatabindjsondemo/JacksonDatabindJsonDemoApplication.java
@@ -1,13 +1,36 @@
 package com.example.jacksondatabindjsondemo;
 
+import com.example.jacksondatabindjsondemo.domain.Student;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 public class JacksonDatabindJsonDemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(JacksonDatabindJsonDemoApplication.class, args);
+
+
+		try {
+
+			// create object mapper
+			ObjectMapper mapper = new ObjectMapper();
+
+			//read json file and convert to Java POJO
+			Student myStudent = mapper.readValue(new File("data/sample-lite.json"), Student.class);
+
+			//print
+			System.out.println(myStudent);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
---
+++ src/main/java/com/example/jacksondatabindjsondemo/domain/Student.java
@@ -0,0 +1,12 @@
+package com.example.jacksondatabindjsondemo.domain;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private boolean active;
+}
